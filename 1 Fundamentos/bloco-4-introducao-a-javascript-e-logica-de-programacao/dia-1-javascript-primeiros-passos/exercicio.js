32879c4c73f44546039bfcd5171670fec2617f34
vagner <vagnerbritz@gmail.com>
2021-11-05 21:08:04 -0300

    Exercício 4.1 parcial questão 1 a 3

---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio.js
@@ -0,0 +1,82 @@
+/*console.log("Questão 1 - Aritmética básica\n");
+
+const a = 120;
+const b = 45;
+
+// Programa 1 Adição
+resultado = (a+b);
+console.log("A SOMA de ",a,"e", b, "é => ",resultado," !");
+
+console.log("-----------------------------------------------");
+
+// Programa 2 Subtração
+resultado = (a-b);
+console.log("A SUBTRAÇÃO ",a,"e", b, "é => ",resultado," !");
+console.log("-----------------------------------------------");
+
+// Programa 3 Multiplicação
+resultado = (a*b);
+console.log("A MULTIPLICAÇÃO de ",a,"e", b, "é => ",resultado," !");
+console.log("-----------------------------------------------");
+
+// Programa 4 Divisão
+resultado = (a/b);
+console.log("A DIVISÃO de ",a,"e", b, "é => ",resultado," !");
+console.log("-----------------------------------------------");
+
+// Programa 5 Módulo(resto)
+resultado = (a%b);
+console.log("O resto de ",a,"e", b, "é => ",resultado," !");
+console.log("-----------------------------------------------");
+// 45 cabe 2x no 120 e sobra 30 o resto é 30, exatamente issa é a função do %. 
+
+//Questão 2
+console.log("Questão 2 - Maior de Dois números\n");
+
+const c = 25;
+const d = 65;
+
+if (c>d) {
+    console.log('O c é maior que d');
+}
+else if (c===d) {
+    console.log('O c é igual a d');
+}
+else {
+    console.log('O d é maior que c');
+}
+
+//Questão 3
+console.log("Questão 3 - Maior de 3 números\n");*/
+const e = 188;
+const f = 183;
+const g = 183;
+
+//praticamente tudo poderia ser resumido nessas duas linhas abaixo mas....como é para treinar a lógica tbm..
+var maior = Math.max(e,f,g);
+console.log(maior);
+//*/
+/*if (e===f && e==g) {
+    console.log("Eles tem o mesmo valor");
+}
+else if (e>f && e>g) {
+    console.log("O E é maior");
+}
+else if (f>e && f>g) {
+    console.log("O F é o maior");
+}
+else if (g>e && g>f){
+    console.log("O G é maior");
+}
+else if (e===f && e>g){
+    console.log("E e F são iguais e maiores que G");
+}
+else if (f===g && f>e) {
+    console.log("F e G são iguais e maiores que E");
+}
+else {
+    console.log("G e E são iguais e maiores que F")
+}
+*/
+//Questão 4
+console.log("Questão 4 - Maior de 3 números\n");
