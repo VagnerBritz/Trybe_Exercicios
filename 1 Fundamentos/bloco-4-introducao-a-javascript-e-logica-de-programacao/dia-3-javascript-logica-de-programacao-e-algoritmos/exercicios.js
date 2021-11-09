ac9b7550a2187ce7350d59b2f13a20038fd3b90c
vagner <vagnerbritz@gmail.com>
2021-11-09 16:10:14 -0300

    Exercicios de logica

---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,34 @@
+ /*
+ //Questão 1:
+let n= 5;
+for (let index = 0; index < n; index+=1) {
+    console.log("*****");
+    
+}
+ 
+ //Questão 2:
+ let n="*";
+ for (let index = 0; index < 5; index++) {
+     
+     console.log(n);    
+     n+="*";
+}
+//Questão 3:
+let n="*"
+let espaco=["    ","   ","  "," ",""];
+ for (let index = 0; index < 5; index+=1) {
+     
+     console.log(espaco[index], n);    
+     n+="*";
+     
+ }*/
+//Questão 4:
+let n="*";
+let espaco=["  "," ",""];
+//let espDir=["  ",];
+for (let index = 0; index < 3; index+=1) {
+    console.log(espaco[index],n);
+    n+="**";
+    
+}
+ 
