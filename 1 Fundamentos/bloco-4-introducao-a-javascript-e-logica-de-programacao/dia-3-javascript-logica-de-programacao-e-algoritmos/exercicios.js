081decfb28eb67311d668e6d4da08552f7927c11
vagner <vagnerbritz@gmail.com>
2021-11-11 20:19:42 -0300

    refatoraçao parcial

--- 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,18 +1,28 @@
- /*
- //Questão 1:
-let n= 5;
-for (let index = 0; index < n; index+=1) {
-    console.log("*****");
-    
-}
  
- //Questão 2:
- let n="*";
- for (let index = 0; index < 5; index++) {
-     
-     console.log(n);    
-     n+="*";
+/* //Questão 1: 
+let n= 5; 
+//consultei as respostas do colega Bruno Zoline, em 11/11, no slack
+
+for (let index = 0; index < n; index+=1) {
+    let print ="";
+    for (let ast =0; ast < n; ast+=1) {
+        print = print+"*";
+    }
+    console.log(print);
 }
+
+ */
+ //Questão 2:
+ let n =5;
+ for (let index = 0; index < n; index+=1) {
+    let print ="";
+    for (let ast =0; ast <= index; ast+=1) {
+        print = print+"*";
+    }
+    console.log(print);
+} 
+
+/*
 //Questão 3:
 let n="*"
 let espaco=["    ","   ","  "," ",""];
@@ -21,7 +31,7 @@ let espaco=["    ","   ","  "," ",""];
      console.log(espaco[index], n);    
      n+="*";
     }
-*/
+
 //Questão 4:
 let n="*";
 let espaco=["      ","     ","    ","   ","  "," ",""];
@@ -31,57 +41,4 @@ for (let index = 0; index < 7; index+=1) {
     n+="**";
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let ingredientes = ["queijo","presunto","maionese"];
-let qtd =10;
-let sacola =[];
-ingredientes.splice()
-
-for (let index = 0; index < 3; index+=1) {
-    //console.log("executou fora", index);
-    let pao=[];
-
-    for (let index = 0; index < ingredientes.length; index+=1) {
-       // console.log("executou dentro", index);
-       pao.push(ingredientes[index])
-        
-    }
-    sacola.push(pao);
-}
-console.log("sacola: ", sacola);
-
-let numeros= [1,2,3,4,5,6,7,8]
-let retorno = numeros.indexOf('2');
-numeros.splice(retorno,1);
-console.log(numeros);
-
-// EXEMPLO FOR DENTRO DE FOR
-let qtd = 10;
-let ingredientes = ["queijo","presunto","maionese","coca-cola"];
-let sacola = [];
-for(let index = 0; index < 10; index+=1){
-//console.log('exectuou fora: ', index)
-
-let pao = [];
-
-for( let index = 0; index < ingredientes.length; index+=1){
-// console.log('exectuou dentro: ', index);
-pao.push(ingredientes[index])
-}
-sacola.push(pao);
-
-} 
-console.log("sacola: ", sacola);*/
+*/
