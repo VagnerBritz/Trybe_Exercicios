54766e3aabe135bf5ed8ec389a4212489d344828
vagner <vagnerbritz@gmail.com>
2021-11-10 23:48:02 -0300

    exercicios do 1 ou 5

---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -0,0 +1,53 @@
+//Exercício 1
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers.length; index+=1) {
+    console.log(numbers[index]);
+
+};
+//Questão dois - Soma de numeros do arrays
+let soma=0;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let i = 0; i < numbers.length; i+=1)   {
+
+    soma+= numbers[i];
+}
+console.log(soma);
+
+//Questão 3 - Media Aritmetica dos numeros array.
+let soma=0
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let n= numbers.length;
+for (let i = 0; i < numbers.length; i+=1) {
+    soma+=numbers[i];
+}
+let media = soma/n;
+console.log("A média aritmetica dos números da array é:",media);
+
+//Questão 4
+let soma=0
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let n= numbers.length;
+for (let i = 0; i < numbers.length; i+=1) {
+    soma+=numbers[i];
+}
+let media = soma/n;
+    if (media >20) {
+        console.log("Valor maior que 20");
+    }
+    else {
+        console.log("Valor menor que 20");
+    }
+
+//Questão 5
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorN=0;
+for (let index = 0; index < numbers.length; index+=1) {
+    if (maiorN < numbers[index]) {
+        maiorN=numbers[index]        
+    }
+}
+console.log("o maior número é o:",maiorN);
+*/
