1913c6999f6ff17fa60b0136005c7acac4f22a96
vagner <vagnerbritz@gmail.com>
2021-11-13 21:21:49 -0300

    exercicio 4.4

---
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio_objetos_e_funcao.js
@@ -0,0 +1,31 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: "sim",
+  };
+ /* //Exercícico 1
+  console.log("Bem-vinda,",info.personagem);
+  //Exercicio 3
+  for (const key in info) {
+      console.log(key);
+  }
+  //Exercicio 4
+  for (const key in info) {
+      console.log(info[key]);
+  }
+  //Exercicio 5
+  */
+let info2 = {
+    personagem:"Tio Patinhas",
+    origem:["Christmas on Bear Mountain","Dell's Four Color Comics #178"],
+    nota:"O último MacPatinhas",
+    recorrente:"sim"
+}
+//console.log(info.personagem, "e",info2.personagem);
+//}
+let mensagem ="";
+//for (const key in info2) {
+
+console.log(mensagem);
+console.log(info.origem,"e",info2.origem[0]+ "e" +info2.origem[1]);
