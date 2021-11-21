ce38272f91e77ffcab5c9710cece17b2c9d9dc8d
vagner <vagnerbritz@gmail.com>
2021-11-21 19:50:54 -0300

    termino exercicios

--- 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,6 +1,8 @@
 // 1 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 let localizarLocal = document.querySelector('body');
 let criarH1 = document.createElement('h1');
+criarH1.className = 'title'
+
 criarH1.innerText ='Exercício 5.2 - JavaScript DOM';
 localizarLocal.appendChild(criarH1);
 
@@ -33,6 +35,7 @@ local5.appendChild(criarTagSection);
 let local6 = document.querySelector('.main-content');
 let criarTagSection2 = document.createElement('section');
 criarTagSection2.className = 'right-content';
+criarTagSection2.style.marginRight = 'auto'; // inclementado pelo exercicio 2.4
 local6.appendChild(criarTagSection2);
 
 // 7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
@@ -52,16 +55,29 @@ local8.appendChild(ulLi);
 const numeros =['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', '10'];
 
 let localEx8_1 = document.  querySelector('.list-item');
- for (let index = 0; index < numeros.length; index += 1) {
+ for (let index = 0; index > numeros.length; index += 1) {
       let numero = numeros[index];
       let numeroLi = document.createElement('li');
       numeroLi.innerText = numero;
       localEx8_1.appendChild(numeroLi);
  }
 
-// 9   Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
-// let local9 = document.querySelector('.main-content');
-//for (let index =0; index >3; index +=1) {
-//     let h3 = document.createElement('h3');
-//     local9.appendChild(h3);
-//}
+ //9   Adicione 3 tags h3 , todas sendo filhas do main criado no passo 2.
+ let local9 = document.querySelector('.main-content');
+for (let index =0; index <3; index +=1) {
+     let h3 = document.createElement('h3');
+     h3.className = 'description';
+     local9.appendChild(h3);
+}
+// 2.1 Adicione a classe title na tag h1 criada - feito lá em cima;
+// 2.2 Adicione a classe description nas 3 tags h3 criadas - feito acima;
+// 2.3 Remova a section criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+let removerPasso5 = document.querySelector('.left-content');
+removerPasso5.remove();
+//let removerP5 = document.querySelectorAll('.main-content');
+//removerP5.removeChild('.main-content')[1];
+// 2.4   Centralize a section criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto da section ;
+//feito acima.
+// 2.5 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+document.querySelector('.center-content').parentNode.style.backgroundColor = 'green';
+//document.querySelector('.center-content').parentNode.style.backgroundColor = 'green';
