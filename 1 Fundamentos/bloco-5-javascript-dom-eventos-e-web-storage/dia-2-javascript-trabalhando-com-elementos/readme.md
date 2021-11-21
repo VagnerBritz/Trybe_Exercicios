6ab1fd6e2338f75764902559ae16b166fb77a59b
vagner <vagnerbritz@gmail.com>
2021-11-21 00:57:22 -0300

    Exercicios parciais

--- 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/readme.md
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/readme.md
@@ -1,5 +1,6 @@
 O objetivo desses exercícios é colocar em prática o que você acabou de aprender sobre DOM. Por isso, você deve fazer os exercícios utilizando apenas código JavaScript , o qual deve ser inserido entre as tags <script> e </script> .
 Para uma melhor organização, faça commits a cada tarefa concluída. Vamos aos exercícios:
+
 1   Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 2   Adicione a tag main com a classe main-content como filho da tag body ;
 3   Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
---
+++ 1 Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,60 @@
+// 1 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+let localizarLocal = document.querySelector('body');
+let criarH1 = document.createElement('h1');
+criarH1.innerText ='Exercício 5.2 - JavaScript DOM';
+localizarLocal.appendChild(criarH1);
+
+// 2 Adicione a tag main com a classe main-content como filho da tag body ;
+
+let localizarLocal2 = document.querySelector('body');
+let criarMain = document.createElement('main');
+criarMain.className = 'main-content';
+localizarLocal2.appendChild(criarMain);
+
+// 3 Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+let localizarLocal3 =document.querySelector('.main-content');
+let criarSection = document.createElement('section');
+criarSection.className = 'center-content';
+localizarLocal3.appendChild(criarSection);
+
+// 4 Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+let local4 = document.querySelector('.center-content');
+let criarTagP = document.createElement('p');
+criarTagP.innerText = 'Algum texto';
+local4.appendChild(criarTagP);
+
+// 5 Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+let local5 = document.querySelector('.main-content');
+let criarTagSection = document.createElement('section');
+criarTagSection.className = 'left-content';
+local5.appendChild(criarTagSection);
+
+// 6 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+let local6 = document.querySelector('.main-content');
+let criarTagSection2 = document.createElement('section');
+criarTagSection2.className = 'right-content';
+local6.appendChild(criarTagSection2);
+
+// 7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do section criado no passo 5;
+let linkImg = document.querySelector('.left-content');
+let criarLinkImg = document.createElement('img');
+criarLinkImg.className = 'small-image';
+criarLinkImg.src ='https://picsum.photos/200' //source: https://cursos.alura.com.br/forum/topico-inserir-imagem-dentro-do-javascript-89494
+linkImg.appendChild(criarLinkImg);
+
+// 8 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um , dois , três , ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
+
+let local8 = document.querySelector('.right-content');
+let ulLi=document.createElement('ul');
+ulLi.className = 'list-item';
+local8.appendChild(ulLi);
+
+const numeros =['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', '10'];
+
+let localEx8_1 = document.  querySelector('.list-item');
+ for (let index = 0; index < numeros.length; index += 1) {
+      let numero = numeros[index];
+      let numeroLi = document.createElement('li');
+      numeroLi.innerText = numero;
+      localEx8_1.appendChild(numeroLi);
+ }
