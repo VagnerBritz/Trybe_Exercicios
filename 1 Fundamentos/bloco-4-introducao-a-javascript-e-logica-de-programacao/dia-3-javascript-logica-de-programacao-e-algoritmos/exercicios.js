f7a8c3782822c898ffc4ad850d96676b04b00cb7
vagner <vagnerbritz@gmail.com>
2021-12-05 18:08:08 -0300

    alteraçao de codigo

--- 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ 1 Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,17 +1,14 @@
  
-/* //Questão 1: 
+//Questão 1: 
 let n= 5; 
-//consultei as respostas do colega Bruno Zoline, em 11/11, no slack
-
 for (let index = 0; index < n; index+=1) {
-    let print ="";
+    let print ='';
     for (let ast =0; ast < n; ast+=1) {
         print = print+"*";
     }
     console.log(print);
 }
 
- */
  //Questão 2:
  let n =5;
  for (let index = 0; index < n; index+=1) {
@@ -22,15 +19,16 @@ for (let index = 0; index < n; index+=1) {
     console.log(print);
 } 
 
-/*
+
 //Questão 3:
 let n="*"
 let espaco=["    ","   ","  "," ",""];
- for (let index = 0; index < 5; index+=1) {
-     
-     console.log(espaco[index], n);    
+for (let index = 0; index < 5; index+=1) {
+    console.log(espaco[index], n);    
      n+="*";
-    }
+};
+// ou assim que é o CORRETO:
+// figura ao lado by Yuri de Carvalho;
 
 //Questão 4:
 let n="*";
@@ -41,4 +39,16 @@ for (let index = 0; index < 7; index+=1) {
     n+="**";
     
 }
-*/
+// ou assim que é o CORRETO:
+// arquivo de Yuri Carvalho
+let numero = 5;
+for (let horizontal = 1; horizontal <= numero; horizontal += 2) {
+    let linha = '';
+    for(let vertical = 0; vertical < (numero - horizontal)/2; vertical++) {
+        linha = linha + ' ';
+    }
+    for(let vertical2 = 0; vertical2 < horizontal; vertical2 ++) {
+        linha = linha + "*";
+        }
+    console.log(linha);
+};
