607ba9250a538aaff5997ec0d90d035bc7972995
vagner <vagnerbritz@gmail.com>
2021-12-05 16:43:11 -0300

    Inicio dos exercicios

---
+++ 1 Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,10 @@
+const testingScope = (escopo) => (escopo===true) ?
+console.log('Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !') :
+console.log('Não devo ser utilizada fora meu escopo (else)')
+testingScope(true);
+
+// Exercício 2;
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+let 
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
